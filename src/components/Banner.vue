4e66b668e4c7118b62f0ccc1a038c330ca2283fa
Felipe-Gs <felipegs2005@gmail.com>
2024-01-16 11:07:08 -0300

    feat: desenvolvimeto do banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div id="main-banner">
+        <h1>Vehicle inspection control</h1>
+    </div>
+    
+</template>
+
+<script>
+    export default{
+        name: "Banner"
+    }
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url('/public/img/imagem-de-fundo.jpg');
+        background-position: 0 -200px;
+        background-size: cover;
+        height: 550px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    #main-banner h1{
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 20px 40px;
+    }
+</style>
